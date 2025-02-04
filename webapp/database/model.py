24a492df0d7af93da0180df853a8a581d2f8ebc5
Disha <disha.wind@gmail.com>
2025-02-04 13:53:46 +0300

    add relationship

--- webapp/database/model.py
+++ webapp/database/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, BigInteger, String, Float, ForeignKey, DateTime, func
+from sqlalchemy import Column, BigInteger, String, ForeignKey, DateTime, func, Numeric
 from sqlalchemy.orm import DeclarativeBase, relationship
 
 
@@ -9,31 +9,40 @@ class User(Base):
     __tablename__ = "user"
 
     id = Column(BigInteger, primary_key=True)
-    email = Column(String)
-    password_hash = Column(String)
+    email = Column(String, nullable=False, unique=True)
+    password_hash = Column(String, nullable=False)
     full_name = Column(String)
+
     admin = relationship("Admin", back_populates="user", uselist=False)
     accounts = relationship("Account", back_populates="user")
+
 
 class Admin(Base):
     __tablename__ = "admin"
 
-    user_id = Column(ForeignKey(User.id), primary_key=True)
+    user_id = Column(BigInteger, ForeignKey("user.id"), primary_key=True)
+
+    user = relationship("User", back_populates="admin")
+
 
 class Account(Base):
     __tablename__ = "account"
 
     id = Column(BigInteger, primary_key=True)
-    user_id = Column(ForeignKey(User.id))
-    balance = Column(Float)
+    user_id = Column(BigInteger, ForeignKey("user.id"), nullable=False)
+    balance = Column(Numeric(precision=12, scale=2), default=0)
     created_at = Column(DateTime, server_default=func.now())
-    payments = relationship("Payment", back_populates="payment")
+
+    user = relationship("User", back_populates="accounts")
+    payments = relationship("Payment", back_populates="account")
+
 
 class Payment(Base):
     __tablename__ = "payment"
 
     transaction_id = Column(BigInteger, primary_key=True)
-    account_id = Column(ForeignKey(Account.id))
-    amount = Column(Float)
+    account_id = Column(BigInteger, ForeignKey("account.id"), nullable=False)
+    amount = Column(Numeric(precision=12, scale=2), nullable=False)
     created_at = Column(DateTime, server_default=func.now())
     
+    account = relationship("Account", back_populates="payments")
